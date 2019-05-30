d856b2067c6936e925b4c93feec86fad4da6b902
AhmedAl-Doori <aaldoor00@charlottesvilleschools.org>
2019-05-30 15:26:49 -0400

    Modified FUll by cleaning up the data and controlling the servos better

--- FULL.py
+++ FULL.py
@@ -20,36 +20,69 @@ myServo = Servo(servoPin, min_pulse_width=minPulse, max_pulse_width=maxPulse)
 myAlarm = PWMLED(alarmPin)
 mySensor = DistanceSensor(echo=echoPin, trigger=trigPin, max_distance=2)
 myAcc = Adafruit_LSM303.LSM303()
+xList = []
+valList = []
+disList = []
+
 #function to convert the error to the servo
 def map(amount):
     mappedAmount = 1000*(amount - 0) * (maxPulse - minPulse) / (1000 + 1000) + minPulse
     return mappedAmount
+
 #function to turn on the alarm
 def turnTemp():
     myAlarm.value = 1
-    sleep(1)
+    sleep(0.5)
     myAlarm.value = 0
 
 #function to calculate distance and fire alarm
 def fire():
-    if mySensor.distance==0.0:
-        print(mySensor.distance)
-        turnTemp()
+    disList.append(mySensor.distance)
+    if len(disList)==10:
+        avg = sum(disList)/10
+        if avg<0.1:
+            #print(mySensor.distance)
+            turnTemp()
+        else:
+            #print(mySensor.distance)
+            myAlarm.value = 0
+        disList.clear()
     else:
-        print(mySensor.distance)
-        myAlarm.value = 0
+        pass
 #function to calculate error in tilt and stabilize
 def orient(targetTilt):
     accel, mag = myAcc.read()
     x, y, z = accel
+    xList.append(x)
     #print("X = {0}, Y = {1}, Z = {2}".format(x,y,z)),
-    error = abs(abs(x) - targetTilt)
-    mappedError = map(error)
-    if (z!=0):
-        value = (-1)*mappedError*z/abs(z)
+
+    if len(xList) == 10: 
+        avg = sum(xList)/10
+        error = abs(abs(avg) - targetTilt)
+        mappedError = map(error)
+        
+        if (z!=0):
+            value = (-1)*mappedError*z/abs(z)
+            valList.append(value)
+        else:
+            value = 0
+            valList.append(value)
+
+        print(valList)
+
+        if len(valList) == 10:
+            if abs(valList[0]-valList[9])>0.1:
+                myServo.value = valList[9]
+                print(valList[9])
+            else:
+                myServo.value = None
+            valList.clear()
+        else:
+            pass
+        #print(xList, "and", avg)
+        xList.clear()
     else:
-        value = 0
-    myServo.value = value
+        pass
     #print(" Error = {0}, Mapped = {1}, Value = {2}".format(error, mappedError,value))
 
 #calibrat the servo and test the bounds
@@ -64,4 +97,4 @@ time.sleep(5)
 while True:
     orient(targetTilt)
     fire()
-    sleep(0.1)
+    
